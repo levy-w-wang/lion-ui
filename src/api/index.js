5c181c8d935dfd35e21f77bbb960dfaf23d5f1cf
Levy <levywang123@gmail.com>
2021-01-13 19:37:15 +0800

    文件引用方式修改

--- src/api/index.js
+++ src/api/index.js
@@ -1,27 +1,29 @@
 /*
  * 接口统一集成模块
  */
-// import * as user from '@/api/modules/user'
-// import * as system from '@/api/modules/system'
+
+const path = require('path')
+const files = require.context('@/api/modules', false, /\.js$/)
+const modules = {}
+files.keys().forEach(key => {
+    const name = path.basename(key, '.js')
+    modules[name] = files(key).default || files(key)
+})
+export default modules
+
+// import user from '@/api/modules/user'
+// import system from '@/api/modules/system'
+// import menu from '@/api/modules/menu'
+// import role from '@/api/modules/role'
+// import quartz from '@/api/modules/quartz'
+// import quartzlog from '@/api/modules/quartzlog'
 
 // // 默认全部导出
 // export default {
-//     ...user,
-//     ...system
+//     user,
+//     system,
+//     menu,
+//     role,
+//     quartz,
+//     quartzlog,
 // }
-import user from '@/api/modules/user'
-import system from '@/api/modules/system'
-import menu from '@/api/modules/menu'
-import role from '@/api/modules/role'
-import quartz from '@/api/modules/quartz'
-import quartzlog from '@/api/modules/quartzlog'
-
-// 默认全部导出
-export default {
-    user,
-    system,
-    menu,
-    role,
-    quartz,
-    quartzlog,
-}
--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,23 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-import user from './modules/user'
-import app from './modules/app'
+// import user from './modules/user'
+// import app from './modules/app'
 
+const path = require('path')
+const files = require.context('./modules', false, /\.js$/)
+const modules = {}
+files.keys().forEach(key => {
+    const name = path.basename(key, '.js')
+    modules[name] = files(key).default || files(key)
+})
+// const store = new Vuex.Store({
+//     modules: {
+//         user: user,
+//         app: app,
+//     },
+// })
 const store = new Vuex.Store({
-    modules: {
-        user: user,
-        app: app,
-    },
+    modules: modules,
 })
 export default store
